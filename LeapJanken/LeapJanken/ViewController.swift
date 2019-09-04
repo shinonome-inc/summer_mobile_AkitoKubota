b50fcc44a6f44c25be90f2f94d8e7fa6a506d5b0
Akito Kubota <binrenwatian@gmail.com>
2019-09-04 13:41:30 +0900

    閏年とじゃんけんのソースコード

---
+++ LeapJanken/LeapJanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeapJanken
+//
+//  Created by 窪田　彬人 on 2019/09/04.
+//  Copyright © 2019 窪田　彬人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
