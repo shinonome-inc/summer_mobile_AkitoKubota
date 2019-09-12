2cb2d535d61efb1c1b17ec604505673749b87717
Akito Kubota <binrenwatian@gmail.com>
2019-09-12 10:48:37 +0900

    App進捗

---
+++ LeapYearApp/LeapYearApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  LeapYearApp
+//
+//  Created by 窪田　彬人 on 2019/09/09.
+//  Copyright © 2019 窪田　彬人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var LeapLabel: UILabel!
+
+    @IBOutlet weak var userInputTextField: UITextField!
+    
+    override func  viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func decide(_ sender: Any) {
+    
+        guard let Age = userInputTextField.text,
+            let num = Int(Age)else{
+                LeapLabel.text = "表示できません"
+                return
+        }
+        // userInputTextField.textの値をIntに変換してあげる
+        LeapLabel.text = LeapYear(num)
+        
+    }
+    
+
+    func LeapYear(_ year: Int) -> String{
+        let LeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        if LeapYear{
+            return "閏年です。"
+        }else{
+            return "閏年ではありません。"
+        }
+    
+}
+}
---
+++ LeapYearApp/LeapYearAppTests/LeapYearAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  LeapYearAppTests.swift
+//  LeapYearAppTests
+//
+//  Created by 窪田　彬人 on 2019/09/09.
+//  Copyright © 2019 窪田　彬人. All rights reserved.
+//
+
+import XCTest
+@testable import LeapYearApp
+
+class LeapYearAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
